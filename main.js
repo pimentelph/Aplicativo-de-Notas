7490dc97ae7b5a5aebacb948f532f007184fd72c
Pedro Henrique Pimentel da Silva <125388149+pimentelph@users.noreply.github.com>
2024-04-29 13:50:21 -0300

    Terminando de implementar as bases!
    Agora as funções básicas do app já estão feitas! Agora falta algumas funções como "local storage" e afins!

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function criarNotas(){
     var divPai = document.getElementById('divPaiNotas');
     var divFilha = document.createElement('div'); //Cria a div das notas
-    var botaoExcluir = document.createElement('button'); //Crio o botão para excluir
     
     divFilha.className = 'divNotas'; //Atribui a classe que eu criei no CSS
 
+    var botaoExcluir = document.createElement('button'); //Crio o botão para excluir
     botaoExcluir.innerText = 'Excluir'; //Coloca um texto dentro do botão
     botaoExcluir.className = 'botao'; //Atribui a classe que eu criei no CSS
     botaoExcluir.style.backgroundColor = "red"; //Mudei a cor do fundo para vermelho
@@ -13,7 +13,33 @@ function criarNotas(){
         divPai.removeChild(divFilha); //Exclui a divFilha da div nota
     }
 
+    var texto = document.createElement('textarea');
+    texto.className = 'texto';
+    texto.readOnly = false;
+    texto.placeholder = "Pode digitar!"
+
+    var botaoEditar = document.createElement('button'); //Crio o botão para excluir
+    botaoEditar.innerText = 'Salvar'; //Coloca um texto dentro do botão
+    botaoEditar.className = 'botao'; //Atribui a classe que eu criei no CSS
+    botaoEditar.style.backgroundColor = "blue"; //Mudei a cor do fundo para azul
+    botaoEditar.style.color = "aliceblue" //Muda a cor da letra para Alice Blue
+    botaoEditar.onclick = function() { //Manipulador de evento para toda vez que o botão é clicado
+        if (texto.readOnly){
+            texto.readOnly = false;
+            botaoEditar.innerText = 'Salvar';
+            texto.placeholder = "Pode digitar!"
+        } else {
+            texto.readOnly = true;
+            botaoEditar.innerText = 'Editar texto';
+            texto.placeholder = "";
+            texto.style.resize = "none";
+        }
+    }
+
+    
+    divFilha.appendChild(botaoEditar); //Adiciona o botão na div filha
     divFilha.appendChild(botaoExcluir); //Adiciona o botão na div filha
+    divFilha.appendChild(texto); //Adiciona a caixa de texto na div filha
     divPai.appendChild(divFilha); //Adiciona a div filha na div pai
 }
 
